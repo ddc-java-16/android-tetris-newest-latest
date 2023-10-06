2648984a9afa69f5cb6fdc2c9e4be660cf1f69ab
che money <cmelendez15@cnm.edu>
2023-10-06 11:21:20 -0500

    added tetris

--- app/src/main/java/edu/cnm/deepdive/tetris/model/Dealer.java
+++ app/src/main/java/edu/cnm/deepdive/tetris/model/Dealer.java
@@ -7,7 +7,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.Random;
-import javafx.scene.shape.Shape;
+
 
 public class Dealer {
   private final Queue<ShapeType> queue;
