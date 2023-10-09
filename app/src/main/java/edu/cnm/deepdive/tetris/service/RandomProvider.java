b32ee74000f3276a6a370b482f00553a288c1378
che money <cmelendez15@cnm.edu>
2023-10-09 18:54:50 -0500

    playing field repository lambdas

---
+++ app/src/main/java/edu/cnm/deepdive/tetris/service/RandomProvider.java
@@ -0,0 +1,8 @@
+package edu.cnm.deepdive.tetris.service;
+
+import java.util.Random;
+
+public interface RandomProvider {
+  Random provide();
+
+}
--- app/src/main/java/edu/cnm/deepdive/tetris/viewmodel/PlayingFieldViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/tetris/viewmodel/PlayingFieldViewModel.java
@@ -6,13 +6,19 @@ import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import dagger.hilt.android.lifecycle.HiltViewModel;
+import dagger.hilt.android.qualifiers.ApplicationContext;
+import edu.cnm.deepdive.tetris.model.Dealer;
 import edu.cnm.deepdive.tetris.model.Field;
 import edu.cnm.deepdive.tetris.service.PlayingFieldRepository;
 import edu.cnm.deepdive.tetris.service.PreferencesRepository;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Consumer;
 import javax.inject.Inject;
 import org.jetbrains.annotations.NotNull;
 
+@HiltViewModel
 public class PlayingFieldViewModel extends ViewModel implements DefaultLifecycleObserver {
   private final PlayingFieldRepository playingFieldRepository;
   private final PreferencesRepository preferencesRepository;
@@ -20,24 +26,35 @@ public class PlayingFieldViewModel extends ViewModel implements DefaultLifecycle
   private final CompositeDisposable pending;
 
   @Inject
-  PlayingFieldViewModel(Context context,PlayingFieldRepository playingFieldRepository,
+
+  PlayingFieldViewModel(@ApplicationContext Context context,
+      PlayingFieldRepository playingFieldRepository,
       PreferencesRepository preferencesRepository) {
     this.playingFieldRepository = playingFieldRepository;
     this.preferencesRepository = preferencesRepository;
     throwable = new MutableLiveData<>();
     pending = new CompositeDisposable();
+    create();
 
   }
 
   public LiveData<Field> getPlayingField() {
     return playingFieldRepository.getPlayingField();
   }
+  public LiveData<Dealer> getDealer() {
+    return playingFieldRepository.getDealer();
+  }
 
   public LiveData<Throwable> getThrowable() {
     return throwable;
   }
   public void create() {
-    // TODO: 10/6/23 Invoke create in Repository with settings repository.
+   Disposable disposable =playingFieldRepository.create(25, 10, 5, 5 ) // FIXME
+    .subscribe(() -> {
+        },
+        throwable::postValue
+    );
+   pending.add(disposable);
   }
   public void start(){
     // TODO: 10/6/23 Invoke start in repository......
