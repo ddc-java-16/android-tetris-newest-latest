8042ccf3e31ab0d0c09ff459c263c61723645ba6
che money <cmelendez15@cnm.edu>
2023-10-16 15:30:36 -0500

    added entity dao and reository for tha scores.

---
+++ app/src/main/java/edu/cnm/deepdive/tetris/service/ScoreRepository.java
@@ -0,0 +1,35 @@
+package edu.cnm.deepdive.tetris.service;
+
+import androidx.lifecycle.LiveData;
+import edu.cnm.deepdive.tetris.model.dao.ScoreDao;
+import edu.cnm.deepdive.tetris.model.entity.Score;
+import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import java.util.List;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+public class ScoreRepository {
+  private final ScoreDao dao;
+
+  @Inject
+  public ScoreRepository(ScoreDao dao) {
+    this.dao = dao;
+  }
+  public Single<Long> create(Score score) {
+    return dao.insert(score).subscribeOn(Schedulers.io());
+  }
+public Single<Integer> delete(Score score) {
+  return dao.delete(score)
+      .subscribeOn(Schedulers.io());
+}
+public LiveData<Score> read(long id) {
+    return  dao.select(id);
+}
+public LiveData<List<Score>> readAllScorsForUser(long userId) {
+    return dao.selectByPlayerId(userId);
+}
+//TODO define m
+}
